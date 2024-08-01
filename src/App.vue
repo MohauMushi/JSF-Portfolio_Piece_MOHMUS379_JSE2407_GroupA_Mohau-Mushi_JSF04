6d8cb21f9c0880dc1849ee58f39538f9f8b2e1db
Mohau Mushi <david1mushi1@gmail.com>
2024-08-01 13:19:42 +0200

    Imported Header component to App.vue and wrapped it in a div

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
-
+  <div class="min-h-screen bg-gray-100">
+    <Header />
+  </div>
 </template>
 
 <script>
+import Header from "../src/components/Header.vue";
 
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
 </script>
