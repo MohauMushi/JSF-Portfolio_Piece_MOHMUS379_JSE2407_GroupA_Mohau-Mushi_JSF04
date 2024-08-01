029c422b2bac768904e4f394d9a755c9554cf30b
Mohau Mushi <david1mushi1@gmail.com>
2024-08-01 20:42:12 +0200

    Added `<router-view>` to render the matched component for the current route

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="min-h-screen bg-gray-100">
     <Header />
+    <main class="container mx-auto px-4 py-8">
+      <router-view></router-view>
+    </main>
   </div>
 </template>
 
