f9da2b75cf14d26e0328fc98dc78528de0ac654e
Mohau Mushi <david1mushi1@gmail.com>
2024-08-02 10:05:06 +0200

    Renamed router file to router.js,

--- src/router/router.js
+++ src/router/router.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import ProductDetail from '../views/ProductDetail.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/product/:id',
+    name: 'ProductDetail',
+    component: ProductDetail,
+    props: true
   },
   
 ]
