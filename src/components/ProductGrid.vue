f98b110ca8b42c3046d9c382633e915759e5363f
Mohau Mushi <david1mushi1@gmail.com>
2024-08-02 14:15:08 +0200

    Updated grid layout responsiveness

--- src/components/ProductGrid.vue
+++ src/components/ProductGrid.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"
+    class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6"
   >
     <ProductCard
       v-for="product in products"
