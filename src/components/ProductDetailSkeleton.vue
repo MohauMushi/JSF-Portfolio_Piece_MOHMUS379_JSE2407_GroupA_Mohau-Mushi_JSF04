47f86d89ea66d7a34cddd258d08dde029148add2
Mohau Mushi <david1mushi1@gmail.com>
2024-08-02 21:11:02 +0200

    Added ProductDetailSkeleton component for loading state

---
+++ src/components/ProductDetailSkeleton.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-if="show" class="flex flex-col md:flex-row animate-pulse">
+    <!-- Product Image Placeholder -->
+    <div class="md:w-1/2 mb-4 md:mb-0 md:mr-4">
+      <div class="bg-gray-300 h-64 w-full rounded"></div>
+    </div>
+
+    <!-- Product Details Placeholders -->
+    <div class="md:w-1/2">
+      <div class="h-8 bg-gray-300 rounded w-3/4 mb-4"></div>
+      <div class="h-4 bg-gray-300 rounded w-1/4 mb-4"></div>
+      <div class="h-4 bg-gray-300 rounded w-1/2 mb-4"></div>
+      <div class="h-6 bg-gray-300 rounded w-1/4 mb-4"></div>
+      <div class="h-10 bg-gray-300 rounded w-1/3 mb-6"></div>
+      <div class="h-4 bg-gray-300 rounded w-full mb-2"></div>
+      <div class="h-4 bg-gray-300 rounded w-full mb-2"></div>
+      <div class="h-4 bg-gray-300 rounded w-3/4"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductDetailSkeleton",
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
