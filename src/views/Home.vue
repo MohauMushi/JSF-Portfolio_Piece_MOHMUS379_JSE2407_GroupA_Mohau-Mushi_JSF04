b72f6b357eb5e9cfb0f86b0f1d162ae71d957c55
Mohau Mushi <david1mushi1@gmail.com>
2024-08-02 20:43:23 +0200

    Added SkeletonLoader component, implemented loading state, and updated onMounted lifecycle hook

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <ProductGrid :products="products" />
+  <div class="container mx-auto px-4">
+    <div v-if="loading">
+      <SkeletonLoader :count="8" />
+    </div>
+    <div v-else>
+      <ProductGrid :products="products" />
+    </div>
   </div>
 </template>
 
@@ -8,22 +13,33 @@
 import { ref, onMounted, computed } from "vue";
 import { useProductStore } from "../store/ProductStore.js";
 import ProductGrid from "../components/ProductGrid.vue";
+import SkeletonLoader from "../components/SkeletonLoader.vue";
 
 export default {
   name: "Home",
   components: {
     ProductGrid,
+    SkeletonLoader,
   },
   setup() {
     const productStore = useProductStore();
     const products = computed(() => productStore.products);
+    const loading = ref(true);
 
     onMounted(async () => {
-      await productStore.fetchProducts();
+      try {
+        await productStore.fetchProducts();
+      } finally {
+        setTimeout(() => {
+        loading.value = false;
+      }, 1300);
+      }
     });
 
     return {
+      loading,
       products,
+      
     };
   },
 };
