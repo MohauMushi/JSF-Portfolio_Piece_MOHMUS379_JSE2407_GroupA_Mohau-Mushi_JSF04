af645a9bdca4efd37a62dda82299dac9e5fa54d5
Mohau Mushi <david1mushi1@gmail.com>
2024-08-05 11:49:34 +0200

    added Wishlist Component

---
+++ src/views/Wishlist.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1 class="text-2xl font-bold mb-4">Wishlist</h1>
+    <p>This is a placeholder for the Wishlist feature.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Wishlist",
+};
+</script>
