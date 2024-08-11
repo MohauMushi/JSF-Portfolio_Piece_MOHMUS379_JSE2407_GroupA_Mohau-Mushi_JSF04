522e706a400c3f767f41ebcc3356b70bc966ca06
Mohau Mushi <david1mushi1@gmail.com>
2024-08-11 19:38:39 +0200

    Added AuthRequiredModal component and imported necessary dependencies, to show the authRequiredModal

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,19 @@
     <Header />
     <main>
       <router-view></router-view>
+      <AuthRequiredModal
+        :is-open="authStore.authModal.isOpen"
+        :destination="authStore.authModal.destination"
+        @close="authStore.hideAuthModal"
+      />
     </main>
   </div>
 </template>
 
 <script setup>
 import Header from "../src/components/Header.vue";
+import { useAuthStore } from "./store/auth";
+import AuthRequiredModal from "./components/AuthRequiredModal.vue";
+
+const authStore = useAuthStore();
 </script>
