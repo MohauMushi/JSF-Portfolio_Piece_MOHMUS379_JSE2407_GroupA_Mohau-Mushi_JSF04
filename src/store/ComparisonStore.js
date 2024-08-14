cff3b784040c078a5e5fddf99fb977704c8aa50c
Mohau Mushi <david1mushi1@gmail.com>
2024-08-14 21:54:40 +0200

    Added toggle Comparison Checkbox functionality

--- src/store/ComparisonStore.js
+++ src/store/ComparisonStore.js
@@ -5,6 +5,7 @@ export const useComparisonStore = defineStore("comparison", {
   state: () => ({
     comparisonList: [],
     maxComparisonItems: 4,
+    comparisonCheckboxes: {},
   }),
   actions: {
     async addToComparison(productId) {
@@ -17,6 +18,7 @@ export const useComparisonStore = defineStore("comparison", {
           const product = await productStore.fetchProductById(productId);
           if (product) {
             this.comparisonList.push(product);
+            this.comparisonCheckboxes[productId] = true;
           } else {
             console.error('Product not found');
           }
@@ -27,9 +29,21 @@ export const useComparisonStore = defineStore("comparison", {
       this.comparisonList = this.comparisonList.filter(
         (product) => product.id !== productId
       );
+      delete this.comparisonCheckboxes[productId];
     },
     clearComparison() {
       this.comparisonList = [];
+      this.comparisonCheckboxes = {};
+    },
+    async toggleComparisonCheckbox(productId) {
+      if (this.comparisonCheckboxes[productId]) {
+        this.removeFromComparison(productId);
+      } else {
+        await this.addToComparison(productId);
+      }
+    },
+    isInComparison(productId) {
+      return !!this.comparisonCheckboxes[productId];
     },
   },
 });
